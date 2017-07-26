31061157743aa7c22c1c40a3fc493df4f64f0247
梅书豪 <meishuhao@cyou-inc.com>
2017-07-26 19:40:31 +0800

    add include

--- EventCenter.h
+++ EventCenter.h
@@ -16,35 +16,27 @@
 #include <unordered_map>
 #include <algorithm>
 #include <functional>
-
+#include "Observer.h"
 
 class EventCenter : public Singleton<EventCenter> {
 public:
 	template<typename ObserverWrapperType, typename PerformType = ObserverWrapperType::perform_type>
-	void subscribe(Observer & observer, PerformType cb);
+	void subscribe(Observer & observer, PerformType cb){
+		typedef ObserverWrapper<ObserverWrapperType>::DataType _Dt;
+		std::shared_ptr< _Dt > wrapper_ptr = std::make_shared<_Dt>(&observer, cb);
+		ObserverWrapperContainer<ObserverWrapperType>::instance().add(wrapper_ptr);
+	}
 	template<typename ObserverWrapperType>
-	void unsubscribe(uint64_t _observer_id);
+	void unsubscribe(uint64_t _observer_id){
+		ObserverWrapperContainer<ObserverWrapperType>::instance().del(_observer_id);
+	}
 public:
 	template<class ObserverWrapperType, typename ...Args>
-	void notify(Args ...args);
+	void notify(Args ...args){
+		ObserverWrapperContainer<ObserverWrapperType>::instance().perform(args ...);
+	}
 };
 
-template<class ObserverWrapperType, typename ...Args>
-void EventCenter::notify(Args ...args){
-	ObserverWrapperContainer<ObserverWrapperType>::instance().perform(args ...);
-}
-
-template<typename ObserverWrapperType, typename PerformType /*= ObserverWrapperType::perform_type*/>
-void EventCenter::subscribe(Observer & observer , PerformType cb){
-	typedef ObserverWrapper<ObserverWrapperType>::DataType _Dt;
-	std::shared_ptr< _Dt > wrapper_ptr = std::make_shared<_Dt>(&observer, cb);
-	ObserverWrapperContainer<ObserverWrapperType>::instance().add(wrapper_ptr);
-}
-
-template<typename ObserverWrapperType>
-void EventCenter::unsubscribe(uint64_t _observer_id){
-	ObserverWrapperContainer<ObserverWrapperType>::instance().del(_observer_id);
-}
 #define g_event_center EventCenter::instance()
 
 #endif //FILE_DEF_EVENTCENTER_H_
